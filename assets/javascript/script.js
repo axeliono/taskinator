aaa6e529c88b1e38abb4e61dd6adc59033aaa757
Axeliono <69375446+axeliono@users.noreply.github.com>
2020-10-14 19:19:50 -0500

    sending data before remote work

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,24 @@
 //instantiate DOM elements
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //function - Add Task on Button Click
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']");
+    console.dir(taskNameInput);
+
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task";
     tasksToDoEl.appendChild(listItemEl);
+    console.log(event);
+
+
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
